affcabe32981c2eff4c9a44f5d815a04f96a57b5
andres <dj.andreps@gmail.com>
2025-05-19 18:20:15 -0500

    andres_ejercicio01_nodejs.js

--- dia04/andres_ejercicio01_nodejs.js
+++ dia04/andres_ejercicio01_nodejs.js
@@ -1,1 +1,19 @@
-var datos =5
+//primer ejem. hola mundo declarando 
+var mensaje="hola mundo"
+console.log(mensaje)
+// segundo ejem.
+var miEntero=1
+var miDecimal=1.22
+// variables
+var si=true
+var no=false
+// variable tipo string
+var dato="esto es una cadena de caracteres"
+// var indefinido 
+var dat=undefined
+console.log(miEntero)
+console.log(miDecimal)
+console.log(si)
+console.log(no)
+console.log(dato)
+console.log(dat)
