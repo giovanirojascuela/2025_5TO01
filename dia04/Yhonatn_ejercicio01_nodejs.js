faab8d73972a170e27d21818094d7c7cbfac11fd
yhonatan <yoelchoquepata90@gmail.com>
2025-05-20 14:02:39 -0500

    Subiendo el archivo js

---
+++ dia04/Yhonatn_ejercicio01_nodejs.js
@@ -0,0 +1,19 @@
+// primer ejemplo hola mundo declarando con variables con var
+var mensajes = "YOEL YHONATAN CHOQUEPATA MAMANI"
+console.log(mensaje)
+// segundo ejemplo tipo de variables
+var miEntero=1
+var miDecimal=1.22
+// variables boolenas
+var si = true
+var no = false
+// variable de tipo string
+var datos="Esto es una cadena de caracteres"
+// valor definido
+var dat=undefined
+console.log(miEntero)
+console.log(miDecimal)
+console.log(si)
+console.log(no)
+console.log(datos)
+console.log(dat)
