96798a35d8e9dcb61c7d8c83015c53782064ac68
Gio <giovanirojascuela@gmail.com>
2025-05-24 18:27:54 -0500

    Trabajo de API Node Gio

---
+++ dia06/Giovani/servidorapi/index.js
@@ -0,0 +1,64 @@
+import express from 'express';
+import fs from 'fs';
+const app=express();
+//Funcion de Lectura de archivo de datos
+const leerArchivo=()=>{
+    try{
+        const datos=fs.readFileSync("./bd.json");
+        //console.log(JSON.parse(datos));
+        return JSON.parse(datos)
+    } catch(error){
+        console.log(error);
+    } 
+};
+//Funcion de Escritura de archivo de datos
+//leerArchivo();
+const escribirArchivo=(data)=>{
+    try{
+        fs.writeFileSync("./bd.json",JSON.stringify(data));
+    } catch(error){
+        console.log(error);
+    } 
+};
+/*Crear rutas*/
+app.get("/",(req,res)=>{
+    res.send("Bienvenido a mi API Gio - Desarrollo");
+});
+//Vamos creando un CRUD API
+app.get("/libros",(req,res)=>{
+    const data=leerArchivo();
+    res.json(data.libros);
+});
+//Lectura por ID
+app.get("/libros/:id",(req,res)=>{
+    const data=leerArchivo();
+    const id=parseInt(req.params.id);
+    const libro=data.libros.find((libro)=>libro.id===id);
+    res.json(libro);
+});
+app.post("/libros",(req,res)=>{
+    const data=leerArchivo();
+    const body=req.body;
+    const nuevoLibro={
+        id: data.libros.length+1,
+        ...body,
+    };
+    data.libros.push(nuevoLibro);
+    escribirArchivo(data);
+    res.json(nuevoLibro);
+});
+//Actualizacion
+app.put("/libros/:id",(req,res)=>{
+    const data=leerArchivo();
+    const body=req.body;
+    const id=parseInt(req.params.id);
+    const libroId=data.libros.findIndex((libro)=>libro.id===id);
+    data.libros[libroId]={
+        ...data.libros[libroId],
+        ...body
+    }
+});
+/*Mostrar estado en consola*/
+app.listen(3000,()=>{
+    console.log('Escuchando servidor en puerto 3000')
+});
