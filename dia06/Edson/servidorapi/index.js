474f808163d8f253782408ba7c8047c7c11bad23
Edson <edsonpilco6@gmail.com>
2025-05-24 18:27:54 -0500

    Trabajo de API Node Edson

---
+++ dia06/Edson/servidorapi/index.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import fs, { readFileSync } from 'fs';
+const app=express();
+//funcion de lectura de archivo
+const leerArchivo=()=>{
+    try{
+        const datos=fs.readFileSync('./base_datos.json');
+        //console.log(JSON.parse(datos));
+        return JSON.parse(datos)
+    }catch(error){
+        console.log(error)
+    }
+};
+//leerArchivo();
+//funcion de escritura de archivo
+const escribirArchivo=(data)=>{
+    try{
+        fs.writeFileSync('./base_datos.json',JSON.stringify(data));
+    }catch(error){
+        console.log(error)
+    }
+}
+/*Crear rutas*/
+app.get("/",(req,res)=>{
+    res.send("Bienvenido a mi API Edson - Desarrollo");
+});
+//Vamos creando un crud api
+app.get("/libros",(req,res)=>{
+    const data=leerArchivo();
+    res.json(data.libros);
+});
+//Lectura por id
+app.get("/libros/:id",(req,res)=>{
+    const data=leerArchivo();
+    const id=parseInt(req.params.id);
+    const libro=data.libros.find((libro)=>libro.id===id);
+    res.json(libro);
+});
+app.post("/libros",(req,res)=>{
+    const data=leerArchivo();
+    const body=req.body;
+    const nuevoLibro={
+        id: data.libros.length+1,
+        ...body,
+    };
+    data.libros.push(nuevoLibro);
+    escribirArchivo(data);
+    res.json(nuevoLibro);
+});
+//Actualizacion
+app.put("/libros/:id",(req,res)=>{
+    const data=leerArchivo();
+    const body=req.body;
+    const id=parseInt(req.params.id);
+    const libroId=data.libros.findIndex((libro)=>libro.id===id);
+    data.libros[libroId]={
+        ...data.libros[libroId],
+        ...body
+    }
+});
+
+app.listen(3000,()=>{
+    console.log('Escuchando servidor en puerto 3000')
+    
+});
