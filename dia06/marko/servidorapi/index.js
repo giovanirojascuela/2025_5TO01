04a7206513132cf52bf1e2ff43f3e2c6b430253c
marko-arce <mark_k199@hotmail.com>
2025-05-26 18:40:19 -0500

    fin dia 06 marko-arce

--- dia06/marko/servidorapi/index.js
+++ dia06/marko/servidorapi/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import fs from 'fs';
+import bodyParser from "body-parser";
 const app=express();
+app.use(bodyParser.json());
 //Funcion de lectura de archivos de datos
 const leerArchivo=()=>{
     try{
@@ -59,7 +61,15 @@ app.put("/libros/:id",(req,res)=>{
         ...data.libros[libroId],
         ...body
     }
-})
+});
+app.delete("/libros/:id",(req,res)=>{
+    const data =leerArchivo();
+    const id=parseInt(req.params.id);
+    const libroId=data.libros.findIndex((libro)=>libro.id===id);
+    data.libros.splice(libroId,1);
+    escribirArchivos(data);
+    res.json({message: "Libro eliminado"});
+});
 /*Mostrar estado en console*/
 app.listen(3000,()=>{
     console.log('Escuchando servidor en puerto 3000')
