67112aa461202bc4651541c1b3e39ce7302e5bf4
CristhianA <1526757@senati.pe>
2025-05-27 18:22:32 -0500

    Dia 07 27 de mayo dosmilveinticinco rutas

---
+++ dia07/CristhianA_rutas/controladores/paises.controladores.js
@@ -0,0 +1,15 @@
+export const getPaises = (req,res)=>{
+    res.send("Funcion de Retornar PAISES")
+}
+export const getPais = (req,res)=>{
+    res.send("Funcion de Retornar PAis")
+}
+export const crearPais = (req,res)=>{
+    res.send("Funcion de crear PAISES")
+}
+export const modificarPais = (req,res)=>{
+    res.send("Funcion de modificar PAISES")
+}
+export const eliminarPais = (req,res)=>{
+    res.send("Funcion de eliminar PAISES")
+}
