f2310acde4e053b9dccb5a731bea2e00071f37dd
marko-arce <mark_k199@hotmail.com>
2025-05-27 18:22:33 -0500

    27 ejercicio 07 rutas

---
+++ dia07/marko_rutas/routes/index.js
@@ -0,0 +1,9 @@
+import {Router} from "express";
+import paisesRouter from "./paises.rutas.js"
+const indexRouter=Router();
+const prefix ="/api"
+indexRouter.get(prefix,(req,res)=>{
+    res.send("Aplicacion con rutas")
+});
+indexRouter.use(`${prefix}/paises`,paisesRouter);
+export default indexRouter;
