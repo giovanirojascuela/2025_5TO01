f47ce5c7e11fbd4ad57d58d2bcf8c1aeefb80f2b
vanessa <samacha92@gmail.com>
2025-05-27 18:25:57 -0500

    27 ejercicio 07 sandra_rutas

---
+++ dia07/sandra_rutas/controladores/paises.controladores.js
@@ -0,0 +1,15 @@
+export const getPaises=(req,res)=>{
+    res.send("mostrar paises")
+}
+export const getPais=(req,res)=>{
+    res.send("retornar paises")
+}
+export const crearPais=(req,res)=>{
+    res.send("crear paises")
+}
+export const modificarPais=(req,res)=>{
+    res.send("modificar paises")
+}
+export const eliminarPais=(req,res)=>{
+    res.send("eliminar paises")
+}
