94ee497ed5de14bd90b01f921e8b3d3c6c720fe3
LuisDevInger <luismamanich456@gmail.com>
2025-05-31 00:43:09 -0500

    27 ejercicio 07 Luis_rutas

---
+++ dia07/Luis_rutas/controladores/paises.controlador.js
@@ -0,0 +1,19 @@
+export const getPaises=(req,res)=>{
+    res.send("Mostrar paises")
+}
+
+export const getPais=(req,res)=>{
+    res.send("Retornar pais")
+}
+
+export const crearPais=(req,res)=>{
+    res.send("Creo pais")
+}
+
+export const modificarPais=(req,res)=>{
+    res.send("Modifico pais")
+}
+
+export const eliminarPais=(req,res)=>{
+    res.send("Elimino pais")
+}
---
+++ dia07/Luis_rutas/src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import indexRouter from '../routes/index.js';
+const app= express();
+
+app.set("port",process.env.PORT||3000);
+
+/**Rutas */
+
+app.use("/",indexRouter);
+
+app.use((req,res)=>{
+    res.send("404 - No existe esa pagina");
+});
+
+app.listen(app.get("port"),()=>{
+    console.log("El servidor corre en el puerto",app.get("port"));
+});
