a03cd666d82cde2ffde3a74bdbb732047b65bb34
Raul <raulcut03@gmail.com>
2025-05-31 11:58:57 -0500

    conexion base de datos

---
+++ dia07/Brian_rutas/models/conexion.js
@@ -0,0 +1,9 @@
+import mysql from "mysql";
+import dbconfig from "./config/database.js";
+var conexion = mysql.createPool({
+    host:dbconfig.HOST,
+    user:dbconfig.USER,
+    password:dbconfig.PASSWORD,
+    database:dbconfig.DB
+})
+module.exports = conexion;
---
+++ dia07/marko_rutas/config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+    HOST: "marko199k.mysql.pythonanywhere-services.com",
+    USER: "marko199k",
+    PASSWORD: "Terrorblades12",
+    DB: "libreta"
+};
