7e49b43b40059792b6133481384e03b904b5c83c
Edson <edsonpilco6@gmail.com>
2025-05-31 11:56:50 -0500

    complementando dia07 Edson

---
+++ dia07/Edson_rutas/config/databases.js
@@ -0,0 +1,6 @@
+module.exports={
+    HOST:"Edson845.mysql.pythonanywhere-services.com",
+    USER:"Edson845",
+    PASSWORD:"ELMERpilco3$",
+    DB:"libreta"
+};
---
+++ dia07/Edson_rutas/controladores/contactos.controlador.js
@@ -0,0 +1,16 @@
+import Contacto from "../models/contactos.model.js"
+
+exports.create=(req,res)=>{
+    if(!req.body){
+        res.status(400).send({message:"vacio"});
+    }
+    const contacto=new Contacto({
+        nombre:req.body.nombre,
+        apellido:req.body.apellido,
+        profesion:req.body.profesion
+    });
+    Contacto.create(contacto,(err,data)=>{
+        if(err){res.status(500).send({message:err.message||"error"});
+        }else {res.send(data);}
+    })
+}
