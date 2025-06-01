17a906b6573ca1ac0b2ada2b38e836a254afa311
BRENDASHIAMIRALOPEZLAYME <167786892+BRENDASHIAMIRALOPEZLAYME@users.noreply.github.com>
2025-06-01 13:49:33 -0500

    Update Brendasuma.mjs

---
+++ dia05/Brendasuma.mjs
@@ -0,0 +1,14 @@
+// Brendasuma.mjs
+
+export function sum(a, b) {
+    return a + b;
+  }
+  
+  export function sub(a, b) {
+    return a - b;
+  }
+  
+  export function mult(a, b) {
+    return a * b;
+  }
+  
