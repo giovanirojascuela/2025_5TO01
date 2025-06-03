9a2a4b8de3d3f2f38e869eb8d054913d6f742543
wise0102 <1524519@senati.pe>
2025-06-03 18:30:18 -0500

    Finalizado backend

--- dia07/Alain_rutas/index.js
+++
@@ -1,8 +0,0 @@
-import {Router} from "express";
-import paisesRouter from "./paises.rutas.js"
-import {buscarAll} from "../controladores/contactos.controlador.js"
-const indexRouter=Router();
-const prefix="/api"
-indexRouter.get(prefix,contacto.buscarAll);
-indexRouter.use('$(prefix)/paises',paisesRouter);
-export default indexRouter;
---
+++ dia07/alain_rutass/controladores/contactos.controlador.js
@@ -0,0 +1,62 @@
+import ContactoModel from  "../models/contactos.model.js";
+
+//const Contacto=require("../models/contactos.model");
+export  const crearContacto=(req,res)=>{
+    if(!req.body){
+        res.status(400).send({message:"vacio"});
+    }
+    const contacto=new Contacto({
+        nombre:req.body.nombre,
+        apellidos:req.body.apellidos,
+        profesion:req.body.profesion
+    });
+    ContactoModel.create(contacto,(err,data)=>{
+        if(err)
+            res.status(500).send({message:
+                err.message||"error"
+        });
+        else res.send(data);
+    });
+};
+export const getContactos=(req,res)=>{
+    ContactoModel.getAll((err,data)=>{
+        if(err){
+            return res.status(500).send({
+            message: err.message||"error"
+            });
+        }
+        res.status(200).send(data);
+    });
+};
+export const eliminarContacto=(req,res)=>{
+    ContactoModel.getAll((err,data)=>{
+        if(err){
+            return res.status(500).send({
+            message: err.message||"error"
+            });
+        }
+        res.status(200).send(data);
+    });
+};
+
+export const modificarContacto=(req,res)=>{
+    ContactoModel.getAll((err,data)=>{
+        if(err){
+            return res.status(500).send({
+            message: err.message||"error"
+            });
+        }
+        res.status(200).send(data);
+    });
+};
+
+export const getContacto=(req,res)=>{
+    ContactoModel.getAll((err,data)=>{
+        if(err){
+            return res.status(500).send({
+            message: err.message||"error"
+            });
+        }
+        res.status(200).send(data);
+    });
+};
