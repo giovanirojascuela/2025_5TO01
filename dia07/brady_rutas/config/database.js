94d5cb5e644883f3badc985c760829a275b8ca52
brady <bradyalvarezquispe@gamil.com>
2025-06-08 18:28:34 -0500

    se borraron mis archivos del dia07

---
+++ dia07/brady_rutas/config/database.js
@@ -0,0 +1,7 @@
+const dbconfig={
+    HOST:"192.168.10.101",
+    USER:"libretausuario",
+    PASSWORD:"libreta",
+    DB:"libreta"
+};
+export default dbconfig;
--- dia07/brady_rutas/src/app.js
+++ dia07/brady_rutas/src/app.js
@@ -1,11 +1,13 @@
-import express from "express";
-import indexRouter from "express/lib/request";  
+import express from 'express';
+import indexRouter from '../routes/index.js';
 const app= express();
 
-app.set("port".process.env.PORT||3000);
-///rutas 
+app.set("port",process.env.PORT||3000);
+///rutas
 app.use("/",indexRouter);
 app.use((req,res)=>{
-    res.send("")
-})
-app.listen(app.get("port"))
+    res.send("404 - No existe esa pagina");
+});
+app.listen(app.get("port"),()=>{
+    console.log("El servidor corre en el puerto",app.get("port"));
+});
